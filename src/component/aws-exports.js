a88d3ebc0fa83f710ef3d72dd3559756ab3c89c1
aarouaoui <79322975+aarouaoui@users.noreply.github.com>
2021-11-25 14:59:16 +0100

    Update aws-exports.js

--- src/component/aws-exports.js
+++ src/component/aws-exports.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var config = {
-    apiKey: 'da2-it4nsxg3m5evfleanl3qhdei7y',
+    apiKey: 'da2-c5nczyqhnbg43elag2buewuzbi',
     HOST: '3pkk6dosbvbzzoxzetipcxa23y.appsync-api.us-east-2.amazonaws.com',
     REGION: 'us-east-2',
     PATH: '/graphql',
